e99ceb7812aa739bf1f5de64c014fa2fe58a9317
Mikhail Bryukhovets <push.uni@gmail.com>
2021-07-03 18:55:07 +0300

    Revert "Search suggestions implementation"
    This reverts commit fe8025dfd9d4fe7b95f4e12f43fbe269bc0cdab4.

--- cheatfilter.h
+++ cheatfilter.h
@@ -4,30 +4,14 @@
 #include <coreplugin/locator/ilocatorfilter.h>
 #include <coreplugin/icore.h>
 //#include <coreplugin/messagemanager.h>
-
-#include <QNetworkAccessManager>
-#include <QNetworkReply>
-#include <QTimer>
-#include <QWaitCondition>
-
-#include <memory>
-
-class QNetworkReply;
-class QMutex;
-class QWaitCondition;
-
 namespace CheatSh {
 namespace Internal {
-
-class Settings;
 
 class CheatFilter : public Core::ILocatorFilter
 {
     Q_OBJECT
 public:
-    CheatFilter(const Settings* settigns);
-    ~CheatFilter();
-    void prepareSearch(const QString &entry) override;
+    CheatFilter();
     QList<Core::LocatorFilterEntry> matchesFor(QFutureInterface<Core::LocatorFilterEntry> &future,
                                          const QString &entry) override;
     void accept(Core::LocatorFilterEntry selection,
@@ -39,16 +23,7 @@ signals:
     void query(const QString& text) const;
 
 private:
-    void sendSearchRequest();
     //
-    std::unique_ptr<QNetworkAccessManager> network_manager_;
-    std::unique_ptr<QNetworkReply> reply_main_;
-    std::unique_ptr<QMutex> net_mutex_;
-    std::unique_ptr<QWaitCondition> request_finished_;
-    const Settings* settings_ = nullptr;
-    bool exiting_ = false;
-    QTimer search_rate_limiter_;
-    QString search_request_;
 };
 
 }}
